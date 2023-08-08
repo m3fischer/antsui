1e3a18d9f6a52f7515f2b31edf2790ec35beda8d
Manuel Fischer <mail@manuelfischer.de>
2023-08-08 22:17:07 +0200

    Bootstrap JavaScript ergänzt

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import Head from "next/head";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  
+    return (
+      <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+      </Head>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+                crossorigin="anonymous">        
+        </Script>
+        <Component {...pageProps} /></>)
   }
   
